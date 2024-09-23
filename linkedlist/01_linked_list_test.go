07b7db5c80ec0f5f738321216ac1b5c1ec4f1fe9
SSureshC <sureshchandak1@gmail.com>
2024-09-23 18:57:03 +0530

    Doubly Linked List

--- linkedlist/01_linked_list_test.go
+++ linkedlist/01_linked_list_test.go
@@ -31,4 +31,24 @@ func TestSingleLinkedList(t *testing.T) {
 
 func TestDoublyLinkedList(t *testing.T) {
 
+	var linkedlist LinkedList = &DoublyLinkedList{
+		head: nil,
+		tail: nil,
+	}
+
+	linkedlist.insert(10)
+	linkedlist.insert(20)
+	linkedlist.insert(30)
+	linkedlist.printLinkedList()
+	linkedlist.insertAtHead(9)
+	linkedlist.printLinkedList()
+	linkedlist.insertAtTail(40)
+	linkedlist.printLinkedList()
+	linkedlist.insertAtPosition(25, 4)
+	linkedlist.insertAtPosition(50, 10)
+	linkedlist.printLinkedList()
+	linkedlist.deleteAtPosition(4)
+
+	linkedlist.printLinkedList()
+	fmt.Println("Size:", linkedlist.size())
 }
