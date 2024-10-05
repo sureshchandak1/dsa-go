e0a534d74ca522b03c970c9e5f693ac0a0c8a0f5
SSureshC <sureshchandak1@gmail.com>
2024-10-05 20:45:06 +0530

    Delete middle element from stack

--- stack/01_stack_test.go
+++ stack/01_stack_test.go
@@ -1,6 +1,7 @@
 package stack
 
 import (
+	"container/list"
 	"fmt"
 	"testing"
 )
@@ -50,4 +51,39 @@ func TestTwoStack(t *testing.T) {
 	fmt.Println("Pop1:", s.pop1())
 	fmt.Println("Pop2:", s.pop2())
 	s.print()
+}
+
+func TestReverseString(t *testing.T) {
+	fmt.Println(reverseString("abc"))
+	fmt.Println(reverseString("love"))
+}
+
+func TestDeleteMiddleElement(t *testing.T) {
+	stack := list.New()
+	stack.PushBack(10)
+	stack.PushBack(20)
+	stack.PushBack(30)
+	stack.PushBack(40)
+	stack.PushBack(50)
+	stack.PushBack(60)
+	stack.PushBack(70)
+
+	deleteMiddleElement(stack)
+
+	printStack(stack)
+
+}
+
+func printStack(stack *list.List) {
+	fmt.Print("[ ")
+	for stack.Len() != 0 {
+		back := stack.Back()
+
+		fmt.Print(back.Value)
+		fmt.Print(" ")
+
+		stack.Remove(back)
+	}
+	fmt.Print("]")
+	fmt.Println()
 }
