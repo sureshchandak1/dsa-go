26a5db53ddc5691fbb46c8cadac20e9ce266d4ee
SSureshC <sureshchandak1@gmail.com>
2024-10-05 17:06:44 +0530

    Implement Two Stacks in single array

--- stack/01_stack_test.go
+++ stack/01_stack_test.go
@@ -30,3 +30,24 @@ func TestStack(t *testing.T) {
 	fmt.Println("Peek:", s.peek())
 	fmt.Println("isEmpty:", s.isEmpty())
 }
+
+func TestTwoStack(t *testing.T) {
+
+	s := twoStack{
+		size:  10,
+		stack: make([]int, 10),
+		top1:  -1,
+		top2:  10,
+	}
+
+	s.push1(11)
+	s.push1(12)
+	s.push2(13)
+	s.push2(14)
+	s.push1(15)
+	s.push2(16)
+	s.print()
+	fmt.Println("Pop1:", s.pop1())
+	fmt.Println("Pop2:", s.pop2())
+	s.print()
+}
