644474b78b2a79ac2268cfb23aced1eda34dfabc
SSureshC <sureshchandak1@gmail.com>
2024-10-16 16:46:30 +0530

    Deque Implement

--- queue/01_queue_test.go
+++ queue/01_queue_test.go
@@ -59,3 +59,45 @@ func TestCircularQueue(t *testing.T) {
 
 	fmt.Println(queue.arr)
 }
+
+func TestDeque(t *testing.T) {
+	queue := newDeque(10)
+	queue.pushRear(10)
+	queue.pushRear(20)
+	queue.pushRear(30)
+	queue.pushRear(40)
+	queue.pushRear(50)
+	queue.pushFront(60)
+	queue.pushFront(70)
+	queue.pushFront(80)
+	queue.pushFront(90)
+	queue.pushFront(100)
+
+	fmt.Println(queue.arr)
+
+	fmt.Println("Front:", queue.getFront())
+	fmt.Println("Rear:", queue.getRear())
+
+	fmt.Println("Pop Front:", queue.popFront())
+	fmt.Println("Pop Rear:", queue.popRear())
+
+	fmt.Println(queue.arr)
+
+	fmt.Println("Pop Rear:", queue.popRear())
+	fmt.Println("Pop Rear:", queue.popRear())
+
+	fmt.Println(queue.arr)
+
+	queue.pushFront(101)
+	queue.pushFront(102)
+
+	fmt.Println(queue.arr)
+
+	fmt.Println("Pop Front:", queue.popFront())
+	fmt.Println("Pop Front:", queue.popFront())
+
+	queue.pushRear(1001)
+	queue.pushRear(2002)
+
+	fmt.Println(queue.arr)
+}
